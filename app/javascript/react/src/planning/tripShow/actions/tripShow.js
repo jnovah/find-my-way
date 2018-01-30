588516d40578a5f0da8bbf0ad9d07b28790ff21e
uncommonAP <aaron.paul_r@outlook.com>
2018-01-30 15:59:52 -0500

    add returns to action creators

--- app/javascript/react/src/planning/tripShow/actions/tripShow.js
+++ app/javascript/react/src/planning/tripShow/actions/tripShow.js
@@ -5,27 +5,39 @@ export const HAS_LEGS = 'NO_LEGS'
 export const COMPLETED_TRIP = 'COMPLETED_TRIP'
 
 let toggleTrip = (id) => {
-  type: TOGGLE_TRIP,
-  id
+  return {
+    type: TOGGLE_TRIP,
+    id
+  }
 }
 
 let fetchTrip = () => {
-  type: FETCH_TRIP
+  return {
+    type: FETCH_TRIP
+  }
 }
 
 let fetchTripSuccess = trip => {
-  type: FETCH_TRIP_SUCCESS,
-  trip
+  return {
+    type: FETCH_TRIP_SUCCESS,
+    trip
+  }
 }
 
 let hasLegs = directions => {
-  type: HAS_LEGS
-  directions
+  return {
+    type: HAS_LEGS,
+    directions
+  }
 }
 
 let completedTrip = () => {
-  type: COMPLETED_TRIP
+  return {
+    type: COMPLETED_TRIP
+  }
 }
+
+
 
 let getTrip = id => dispatch => {
   dispatch(fetchTrip())
@@ -34,13 +46,6 @@ let getTrip = id => dispatch => {
     headers: {"Content-Type": "application/json"}
   }) .then(response => response.json())
   .then(tripObject => {
-    debugger
-    if (!tripObject.trip.has_legs) {
-      dispatch(noLegs(tripObject.directions))
-    }
-    if (tripOject.trip.completed) {
-      dispatch(completedTrip())
-    }
     dispatch(fetchTripSuccess(tripObject.trip))
   })
 }
